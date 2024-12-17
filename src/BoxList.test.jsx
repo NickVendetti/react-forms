88411e6567145432062cf2662f9ccef46044bc84
NickVendetti <nrvendetti@gmail.com>
2024-12-16 22:20:11 -0500

    current

---
+++ src/BoxList.test.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { render, fireEvent, wait } from "@testing-library/react";
+import BoxList from "./BoxList";
+
+function addBox(boxList, height = "2", width = "2", color = "peachpuff") {
+  const heightInput = boxList.getByLabelText("Height");
+  const widthInput = boxList.getByLabelText("Width");
+  const backgroundInput = boxList.getByLabelText("Background Color");
+  fireEvent.change(backgroundInput, { target: { value: color } });
+  fireEvent.change(widthInput, { target: { value: width } });
+  fireEvent.change(heightInput, { target: { value: height } });
+  const button = boxList.getByText("Add a new box!");
+  fireEvent.click(button);
+}
+
+it("renders without crashing", function() {
+  render(<BoxList />);
+});
+
+it
